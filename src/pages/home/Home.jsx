2d227527c88a7d1433c9d1b58dc7b3d04cf82d2a
Your Namekshai-7 <akshai2537@gmail.com>
2023-06-23 12:48:42 +0530

    first commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,29 @@
-import Navbar from "../../components/navbar/Navbar"
-import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
 import "./home.scss"
+import Chart from "../../components/chart/Chart";
+import Features from "../../components/featured/Features";
 
 const Home = () => {
   return (
       <div className='home'>
-          <Sidebar />
-          <div className="homeContainer"><Navbar/></div>
+        <Sidebar />
+        <div className="homeContainer">
+            <Navbar />
+            <div className="widgets">
+              <Widget type="user"/>
+              <Widget type="order"/>
+              <Widget type="earning"/>
+              <Widget type="balance"/>
+            </div>
+            <div className="charts">
+              <Features/>  
+              <Chart />
+            </div>
+        </div>
+          
+        
     </div>
   )
 }
