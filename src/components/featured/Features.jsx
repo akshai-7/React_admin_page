db9f9dd5dc8ffbb01e1988e9d8f20f41c993414a
Your Namekshai-7 <akshai2537@gmail.com>
2023-06-23 13:27:21 +0530

    first commit

--- src/components/featured/Features.jsx
+++ src/components/featured/Features.jsx
@@ -1,5 +1,9 @@
 import "./featured.scss"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 
 const Features = () => {
   return (
@@ -9,7 +13,21 @@ const Features = () => {
             <MoreVertIcon fontSize="small"/>
         </div>
         <div className="bottom">
-            <div className="featuredChart"></div>
+            <div className="featuredChart">
+                <CircularProgressbar value={70} text="70%" strokeWidth={1}/>
+            </div>
+            <p className="title">Total Sales made Today</p>
+            <p className="amount">$420</p>
+            <p className="desc">Previous transactions processing.Last payments may not be included.</p>
+            <div className="summary">
+                <div className="item">
+                    <div className="itemTitle">Target</div>
+                      <div className="itemResult">
+                        <KeyboardArrowDownIcon fontSize="small"/>
+                        <div className="resultAmount">$12.4k</div> 
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
