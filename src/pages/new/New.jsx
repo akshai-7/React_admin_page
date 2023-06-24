9d6b4d6819614ef64d7f4ab8c741191f49d36365
Your Namekshai-7 <akshai2537@gmail.com>
2023-06-24 22:44:44 +0530

    first commit

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -3,42 +3,45 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import "./new.scss";
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
 
-const New = () => {
+const New = ({inputs,title}) => {
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>Add New User</h1>
+          <h1>{title}</h1>
         </div>
         <div className="bottom"> 
           <div className="left"> <img
               src= "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-                 
               alt=""
             /></div>
           <div className="right">
             <form action="">
               <div className="formInput">
-                <label for=""><DriveFolderUploadOutlinedIcon/></label>
-                <input type="text" placeholder="john_doe" />
+                <label htmlFor="file">Image: <DriveFolderUploadOutlinedIcon className="icon"/></label>
+                <input type="file" id="file" style={{display:'none'}} />
               </div>
-              <div className="formInput">
-                <label for="">User Name</label>
-                <input type="text" placeholder="john_doe" />
+
+              {inputs.map((input) =>
+                <div className="formInput" key={input.id}>
+                  <label for="">{ input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
               </div>
-              <div className="formInput">
+              )}
+  
+              {/* <div className="formInput">
                 <label for="">Name and surname</label>
-                <input type="text" placeholder="John Doe" />
+                <input type="text" placeholder="John Doe"/>
               </div>
               <div className="formInput">
                 <label for="">Email</label>  
-                <input type="text" placeholder="john_doe@gmail.com" />
+                <input type="text" placeholder="john_doe@gmail.com"/>
               </div>
               <div className="formInput">
                 <label for="">Phone</label>
-                <input type="text" placeholder="9159679858" />
+                <input type="text" placeholder="+1 234 567 89"/>
               </div>
               <div className="formInput">
                 <label for="">Password</label>
@@ -46,12 +49,12 @@ const New = () => {
               </div>
               <div className="formInput">
                 <label for="">Address</label>
-                <input type="text" placeholder="Coimbatore" />
+                <input type="text" placeholder="Elton St. 216 NewYork"/>
               </div>
               <div className="formInput">
                 <label for="">Country</label>
-                <input type="text" placeholder="India" />
-              </div>
+                <input type="text" placeholder="USA"/>
+              </div> */}
               <button>Send</button>
             </form>
           </div>
